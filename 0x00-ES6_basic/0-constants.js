9524a26c2b544695200cad5080765b86233d510c
calvincetom <calvincetom@outlook.com>
2024-10-05 18:45:41 +0300

    fix idention errors

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,8 +1,8 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
 export function getLast() {
   return ' is okay';
 }
