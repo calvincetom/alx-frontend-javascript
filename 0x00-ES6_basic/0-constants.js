f60dcd4be5b638a620fe4edaf5eebfc6abea184d
calvincetom <calvincetom@outlook.com>
2024-10-05 18:37:45 +0300

    fix test errors

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,4 @@
-export default function taskFirst() {
+export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
   }
@@ -10,6 +10,5 @@ export default function taskFirst() {
   export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-  
     return combination;
   }
