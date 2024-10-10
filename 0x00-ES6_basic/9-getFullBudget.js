5974b452b5b76f440ab713c0d197859cf7dfaa34
calvincetom <calvincetom@outlook.com>
2024-10-10 16:10:04 +0300

    fix more es6 linter errors

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,14 @@
 import getBudgetObject from './7-getBudgetObject';
 
-export default function getFullBudgetObject(income, gdp, capita) {
+const getFullBudgetObject = (income, gdp, capita) => {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(incomeValue) {
-      return `$${incomeValue}`; // Renamed to incomeValue
-    },
-    getIncomeInEuros(incomeValue) {
-      return `${incomeValue} euros`; // Renamed to incomeValue
-    },
+    getIncomeInDollars: (incomeValue) => `$${incomeValue}`,
+    getIncomeInEuros: (incomeValue) => `${incomeValue} euros`,
   };
 
   return fullBudget;
-}
+};
+
+export default getFullBudgetObject;
