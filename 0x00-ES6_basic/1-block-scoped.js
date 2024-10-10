e186aa7d748036b77edebb6fa9f1d6c4a210fd7c
calvincetom <calvincetom@outlook.com>
2024-10-10 15:20:08 +0300

    fix error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
     let task = false;
     let task2 = true;
-
+  
     if (trueOrFalse) {
-        let task = true;
-        let task2 = false;
+      let task = true;  // This 'task' is scoped to the 'if' block
+      let task2 = false; // This 'task2' is scoped to the 'if' block
     }
-
+  
     return [task, task2];
-}
+  }
+  
